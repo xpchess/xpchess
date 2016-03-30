4ef288afb497d20f31c1a8c68226b74a465f3d01
majogl <glatzner@gmail.com>
2016-03-30 23:55:00 +0200

    Update piece.py
    Upravná funkcia pohni. Pridaný parameter obsahujúci hraciu plochu.

--- Pieces/piece.py
+++ Pieces/piece.py
@@ -10,7 +10,7 @@ class piece(object):
     def __repr__(self):
         typ = str(type(self)).split(".")[1][:-2]
         return "{} {}".format(self.color,typ)
-    def pohyb(self,c1,c2):
+    def pohyb(self,c1,c2,field):
         pass
     pass
 
