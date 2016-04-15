9762e6ccc1e63b4855e1658102583ea3df066258
majogl <glatzner@gmail.com>
2016-04-15 20:57:12 +0200

    Modifikácia game.py pre grafický interface
    Vyskusajte si. Malo by ist pohybovat figurkami po grafickej ploche klikanim mysi... Zatial nie je ziadne take, ze kto je na tahu, ale riadi sa to podla pravidiel specifikovanych v pohybovych funkciach.

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+from frame import *
 sys.path[0:0] = [os.path.join(sys.path[0], './pieces')]
 
 p = ["bishop","king","knight","pawn","queen","rook"]
@@ -13,6 +14,7 @@ class game():
     def __init__(self):
         self.field = [[None] * 8 for i in range(8)]
         self.set_up()
+        self.g = frame(self.preloz())
         #sluzi na debug
 
         # pawn - pesiak
@@ -27,6 +29,8 @@ class game():
                       "Black pawn":"♟","Black rook":"♜","Black knight":"♞",
                       "Black bishop":"♝","Black queen":"♛","Black king":"♚",
                       "None":"　"} # pozor, toto nie je obycajna medzera
+        self.coord1,self.coord2 = None,None
+        self.g.c.bind('<Button-1>',self.klik)
         
     def set_up(self):
         self.field[0] = [rook("Black"),knight("Black"),bishop("Black"),queen("Black"),
@@ -62,29 +66,57 @@ class game():
             if self.field[c1[1]][c1[0]].pohyb(c1,c2,self.field):
                 self.field[c2[1]][c2[0]] = self.field[c1[1]][c1[0]]
                 self.field[c1[1]][c1[0]] = None
+                self.g.move(c1,c2)
                 return True
             else:
                 return False
         return False
 
+    def g_pohni(self, c1, c2):
+        c1,c2 = c1,c2
+
+        inBounds = c1[0] in range(0,8) and c1[1] in range(0,8) and c2[0] in range(0,8) and c2[1] in range(0,8)
+        if self.field[c1[1]][c1[0]] is not None and inBounds:
+            if self.field[c1[1]][c1[0]].pohyb(c1,c2,self.field):
+                self.field[c2[1]][c2[0]] = self.field[c1[1]][c1[0]]
+                self.field[c1[1]][c1[0]] = None
+                self.g.move(c1,c2)
+                return True
+            else:
+                return False
+        return False
+
+    def preloz(self):
+        # sluzi na vytvorenie pola na vykreslovanie cez klassu frame
+        ret = [[None] * 8 for i in range(8)]
+        for i in range(8):
+            for j in range(8):
+                if self.field[i][j] is not None:
+                    ret[i][j] = str(self.field[i][j])
+        return ret
+    def klik(self,e):
+        x,y = (e.x-50)//100,e.y//100
+        print(x,y,str(self.field[y][x]))
+        if self.coord1 is None:
+            if self.field[y][x] is not None:
+                print("Setting coord 1")
+                self.coord1 = (x,y)
+        elif self.coord1 is not None and self.coord2 is None:
+            self.coord2 = (x,y)
+            print("Setting coord 2")
+        if self.coord1 is not None and self.coord2 is not None:
+            print("Hybem z {} na {}... Vysledok je {}".format(self.coord1,self.coord2,
+                                                              self.g_pohni(self.coord1,self.coord2)))
+            self.coord1,self.coord2 = None,None
+                
+
+
+
+
+
 
 
 g = game()
-print(g)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
